e15aa844e1f1062992754be3d353e5c5df5d0e55
Chris Cherry <ctcherry@gmail.com>
2011-08-16 23:02:54 -0700

    Rewrite install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DOWNLOAD=`curl -s https://raw.github.com/ctcherry/easy_php_dev/master/download.sh`
-$DOWNLOAD > /dev/null 2>&1
+curl -o /tmp/download_easy_php_dev.sh https://raw.github.com/ctcherry/easy_php_dev/master/download.sh > /dev/null 2>&1
+. /tmp/download_easy_php_dev.sh > /dev/null 2>&1
 ~/.easy_php_dev/control.sh enable
+rm /tmp/download_easy_php_dev.sh
