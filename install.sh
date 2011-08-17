cff360a2a81539a74b6877ed48feda06519337c7
Chris Cherry <ctcherry@gmail.com>
2011-08-16 22:58:55 -0700

    Try old install file method

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-bash < <(curl -s https://raw.github.com/ctcherry/easy_php_dev/master/download.sh)
+DOWNLOAD=`curl -s https://raw.github.com/ctcherry/easy_php_dev/master/download.sh`
+$DOWNLOAD > /dev/null 2>&1
 ~/.easy_php_dev/control.sh enable
