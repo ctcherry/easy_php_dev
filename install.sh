5f940d4cdd917bdf41d2ff24dc33a1d343da22c2
Chris Cherry <ctcherry@gmail.com>
2011-08-16 23:27:08 -0700

    Silence the downloader script on install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 curl -o /tmp/download_easy_php_dev.sh https://raw.github.com/ctcherry/easy_php_dev/master/download.sh > /dev/null 2>&1
 chmod +x /tmp/download_easy_php_dev.sh
-/tmp/download_easy_php_dev.sh
+/tmp/download_easy_php_dev.sh > /dev/null 2>&1
 rm /tmp/download_easy_php_dev.sh
 exec ~/.easy_php_dev/control.sh enable
